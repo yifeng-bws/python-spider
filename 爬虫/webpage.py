501e129af1ec9f53c8884c778963fd26434ff8f6
逸风 <3864912539@qq.com>
2026-01-11 12:47:35 +0800

    修正了bug

--- 爬虫/webpage.py
+++ 爬虫/webpage.py
@@ -10,6 +10,7 @@ def get_info(url):
         return response
     except Exception as e:
         print(f'Error：\n{e}')
+        return None
 
 
 # 实验性功能
@@ -18,16 +19,20 @@ get_all_tags(response, tag)
 用于获取网页全部标签，用bs4库解析
 '''
 def get_all_tags(response, tag):
-    search = BeautifulSoup(response.text, 'html.parser')
-    tags = search.find_all(tag)
-    return tags
+    if response and response.ok:
+        search = BeautifulSoup(response.text, 'html.parser')
+        tags = search.find_all(tag)
+        return tags
+    else:
+        print('无法解析网页内容')
+        return []
 
 
 
 if __name__ == '__main__':
     # 示例操作
     response = get_info('https://python.org/')
-    print(f'状态码：\n{response.status_code}')
+    print(f'状态码：\n{response.status_code if response else "请求失败"}')
 
     if response and response.ok:
         tags = get_all_tags(response, 'p')
